e3b41fb8ddf4fad646898245159743f93300ef0d
Iskren.Stanislavov <iskren.jgd@gmail.com>
2015-10-31 12:28:54 +0200

    gulp, sass,  http folder, package.json

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+// var concat = require('gulp-concat');
+// var uglify = require('gulp-uglify');
+// var rename = require('gulp-rename');
+// var minifyCSS = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+
+var SASSsources = ['./new.scss',];
+var actionsByOrderForDefault = [];
+
+// Compile sass into CSS & auto-inject into browsers
+
+actionsByOrderForDefault.push('html');
+gulp.task('html', function() {
+		return gulp.src(["./new.html","./index.html",])
+	        .pipe(gulp.dest('./http/'))
+	        .pipe(browserSync.stream());
+});
+
+actionsByOrderForDefault.push('sass');
+gulp.task('sass', function() {
+    return gulp.src(SASSsources)
+        .pipe(sourcemaps.init())
+        .pipe(sass({style: 'compact'}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./http/'))
+        .pipe(browserSync.stream());
+});
+
+// // Concatenate & Minify CSS
+// actionsByOrderForDefault.push('minify-css');
+// gulp.task('minify-css', function() {
+//     return gulp.src('./bower_components/bootstrap/dist/css/bootstrap.css')
+//     	.pipe(sourcemaps.init())
+//     	.pipe(minifyCSS())
+//         .pipe(sourcemaps.write())
+//         .pipe(gulp.dest('./assets/css'));
+// });
+
+
+// Run mini server
+actionsByOrderForDefault.push('server');
+gulp.task('server', function() {
+		browserSync.init({
+		server: "./http/",
+        notify: false,
+        reloadDebounce: 5000
+    });
+});
+
+actionsByOrderForDefault.push('watch');
+gulp.task('watch', function(){
+	SASSsources.forEach(function(source){
+		gulp.watch(source, ['sass']);
+	});
+	gulp.watch("./*.html").on('change', function(){
+		gulp.src(["./new.html","./index.html",])
+	        .pipe(gulp.dest('./http/'))
+	        .pipe(browserSync.stream());
+
+		// browserSync.reload();
+	});
+});
+
+
+
+// Static server
+gulp.task('default', actionsByOrderForDefault);
