77a9697b445c10c1e8b7e7968838ad083dfc953d
Iskren.Stanislavov <iskren.jgd@gmail.com>
2015-11-04 01:39:59 +0200

    add classes for page triggering

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 setConfig();//comes from config_paths.js
 define(function (require) {
-    var ScrollMagic = require("scrollmagic/ScrollMagic.min");
+    var Page = function(controller, page){
+		var domE = page.get();
+    	ScrollMagic.Scene.call(this, {triggerElement: domE})
+	    	.setPin(domE).addTo(controller)
+	    	.addIndicators()
+	    	.setTween(this.getPageTweens(page));		
+    };
+    Page.prototype = Object.create(ScrollMagic.Scene.prototype);
+    Page.prototype.getPageTweens = function(page){
+    	return TweenLite.to(page.selector+" p",1.0,{opacity:1, ease:Linear.easeNone, immediateRender:false});
+    };
+
+    var HeaderPage = function(controller){
+    	Page.call(this, controller, $("header"));
+    };
+    HeaderPage.prototype = Object.create(Page.prototype);
+    HeaderPage.prototype.getPageTweens = function(){
+    	return TweenLite.to($("header>h1,header>h2"), 1.0, {opacity:1, ease:Linear.easeNone, immediateRender: false});
+
+    };
+    var SectionPage = Page;
 
 	var Main = function(){
 		ScrollMagic.Controller.call(this,{
@@ -8,24 +28,18 @@ define(function (require) {
 	            triggerHook: 'onLeave'
 	        }
 	    });
-	    this.pages = [].slice.call(
-	    	document.getElementsByTagName("header")
-	    	).concat([].slice.call(
-	    	document.getElementsByTagName("section")
-	    	)
-		);
-	    // console.log(this.pages);
-
-	    this.pages.map(this.addPage, this);
+	    this.pages = [new HeaderPage(this)].concat([
+	    	$("section#contacts"),
+	    	$("section#one"),
+	    	$("section#two"),
+	    	$("section#three"),
+	    	$("section#four"),
+	    	$("section#five"),
+	    	$("section#six"),
+    	].map(function($page){
+    		return new SectionPage(this, $page); 
+    	},this));
 	};
 	Main.prototype = Object.create(ScrollMagic.Controller.prototype);
-	Main.prototype.addPage = function(page){
-		console.log(this, page);
-		new ScrollMagic.Scene({
-		        triggerElement: page
-		    })
-		    .setPin(page)
-		    .addTo(this);		
-	};
 	new Main();
 });
