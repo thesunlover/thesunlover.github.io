de4f878b1a999e2f6a358f92a7375c972cf2cdf7
Iskren Stanislavov <i.stanislavov@astoundcommerce.com>
2022-12-19 12:02:50 +0200

    2022:CHANGE: update IDs to classes, add information about Astound experience

--- localhost.bat
+++ localhost.bat
@@ -1,4 +1,4 @@
 REM python manage.py runserver 0.0.0.0:80
 REM http://unix.stackexchange.com/a/24599
 REM python -m http.server 8000
-python -m SimpleHTTPServer 8000
+c:\python27\python.exe -m SimpleHTTPServer 8000
