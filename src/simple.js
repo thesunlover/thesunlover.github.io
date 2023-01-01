51c3be3b3d9ce59c9d173560da096ea6aa385799
Iskren Stanislavov <i.stanislavov@astoundcommerce.com>
2023-01-01 08:37:40 +0200

    2022:ADD: build with webpack use gsap from npm - new stagger syntax used

--- src/simple.js
+++ src/simple.js
@@ -5,6 +5,13 @@ import gsap from "gsap";
 
 function animate(params) {
     const tl = new gsap.timeline();
-    tl.staggerFrom(document.querySelectorAll('.skill_wrap>.barA img.bar'), 0.5, {width:0},0.5);// w/o English
+    const STAGGER_CONFIG = {
+        duration: 0.5,
+        width: 0,
+        stagger: 0.1,
+    };
+    // w/o English
+    const animatedElements = document.querySelectorAll('.skill_wrap>.barA img.bar');
+    tl.from(animatedElements, STAGGER_CONFIG);
 }
 animate();
