2d3f1f9c7f27c810fbdb22ba20cb3bbab165c60c
Iskren Stanislavov <i.stanislavov@astoundcommerce.com>
2023-02-04 14:38:04 +0200

    2023:ADD: prepare for tick42

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/simple.js',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'scripts'),
     filename: 'main.js',
